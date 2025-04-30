716f344d82f53d1a259179cfca1c8294c88ea37b
EnderVaedorn <162649705+EnderVaedorn@users.noreply.github.com>
2025-04-30 17:49:52 -0500

    Delete vs-config-reset.sh

--- vs-config-reset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CONFIG_FILE=".vs-config"
-INSTALL_DIR=$(whiptail --inputbox "Enter new Vintage Story install directory:" 10 60 3>&1 1>&2 2>&3)
-if [ $? -ne 0 ] || [ -z "$INSTALL_DIR" ]; then
-  echo "Cancelled"
-  exit 1
-fi
-echo "$INSTALL_DIR" > "$CONFIG_FILE"
-whiptail --msgbox "Install directory updated." 8 40
